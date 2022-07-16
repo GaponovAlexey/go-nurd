7580a97497ba6715468165df9a9acaa040ce664e
kekelberi <alexei.gaponov@gmail.com>
2022-07-16 13:50:25 +0300

    save

--- html/main.go
+++ html/main.go
@@ -1,16 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
-func todo(w http.ResponseWriter, r *http.Request) {
+type Book struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Pages  int    `json:"pages"`
+}
+
+func hi(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hi"))
+}
+func Hello(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte("<h1 color=red>Hello</h1>"))
+}
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	books := Book{
+		Title:  "Hello from json",
+		Author: "John",
+		Pages:  1,
+	}
+	w.Write([]byte(fmt.Sprintf("Title:%v\nAuthor:%v", books.Title, books.Author)))
 }
 
 func main() {
-	http.NewServeMux()
-	http.HandleFunc("/", todo)
-
+	http.HandleFunc("/", hi)
+	http.HandleFunc("/Hello", Hello)
+	http.HandleFunc("/book", GetBook)
 	http.ListenAndServe(":3000", nil)
 }

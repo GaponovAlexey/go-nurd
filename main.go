ef2c93c0deacb7b0b79ca112e372d71a5f539fb7
kekelberi <alexei.gaponov@gmail.com>
2022-07-21 17:38:41 +0300

    save

--- main.go
+++ main.go
@@ -10,6 +10,12 @@ import (
 func f(c *gin.Context) {
 	t := time.Now()
 
+
+	if c.Request.Method == "OPTIONS" {
+		return
+		}
+		
+
 	// Set example variable
 	c.Set("examplesssssssssssssss", "12333333333333345")
 

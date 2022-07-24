bf09c1cf796d6c289eb6a5b959d37ecebf0fcec6
kekelberi <alexei.gaponov@gmail.com>
2022-07-24 22:03:47 +0300

    save

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
-	."g/l/pakages"
+	"fmt"
 )
 
 func main() {
-	da := TttF()
-print(da)
+	variadF()
+
+}
+
+func variadF() {
+	fs := []int{9, 7, 4, 2, 1}
+	ts := []int{9, 7, 4, 2, 1}
+	fs = append(fs, ts...)
+	m := (*[10]int)(fs)
+	Shaw(m)
+}
+func Shaw(v *[10]int) {
+	for _, v := range *v {
+		defer fmt.Println(v)
+	}
 }

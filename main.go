eca12d19c460cf5853248145544c0b8330e5c1e1
kekelberi <alexei.gaponov@gmail.com>
2022-07-25 19:30:48 +0300

    save

--- main.go
+++ main.go
@@ -4,20 +4,35 @@ import (
 	"fmt"
 )
 
+type User struct {
+	Id   int64
+	name string
+}
+
 func main() {
-	variadF()
-
-}
-
-func variadF() {
-	fs := []int{9, 7, 4, 2, 1}
-	ts := []int{9, 7, 4, 2, 1}
-	fs = append(fs, ts...)
-	m := (*[10]int)(fs)
-	Shaw(m)
-}
-func Shaw(v *[10]int) {
-	for _, v := range *v {
-		defer fmt.Println(v)
+	users := []User{
+		{1, "Joe"},
+		{2, "Mary"},
+		{11, "Mary"},
+		{22, "Mary"},
+		{4, "Mary"},
 	}
+	fmt.Println(users)
+	dm := make(map[int]User, len(users))
+	fmt.Println(dm)
+	for _, us := range users {
+		if _, ok := dm[int(us.Id)]; !ok {
+			dm[int(us.Id)] = us
+		}
+	}
+	fmt.Println(dm)
+	fmt.Println(fim(2, dm))
+
+}
+
+func fim(id int, dm map[int]User) *User {
+	if v, ok := dm[id]; ok {
+		return &v
+	}
+	return nil
 }
--- pakages/pakage.go
+++ pakages/pakage.go
@@ -1,6 +1,89 @@
 package pakages
 
-func TttF() string {
-	return "conect"
+type User struct {
+	list string
+	name string
+}
 
+var data = map[string]User{
+	{
+		"list": "15",
+		"name": "Nehru Dennis",
+	},
+	{
+		"list": "13",
+		"name": "Carlos Spencer",
+	},
+	{
+		"list": "5",
+		"name": "Deirdre Fisher",
+	},
+	{
+		"list": "19",
+		"name": "Abbot Delgado",
+	},
+	{
+		"list": "7",
+		"name": "Kelly Velasquez",
+	},
+	{
+		"list": "1",
+		"name": "Jessamine Richard",
+	},
+	{
+		"list": "3",
+		"name": "TaShya Roman",
+	},
+	{
+		"list": "9",
+		"name": "Demetrius Levy",
+	},
+	{
+		"list": "1",
+		"name": "Cassidy Leonard",
+	},
+	{
+		"list": "11",
+		"name": "Elizabeth Berger",
+	},
+	{
+		"list": "7",
+		"name": "Ahmed Rich",
+	},
+	{
+		"list": "13",
+		"name": "Preston Whitaker",
+	},
+	{
+		"list": "15",
+		"name": "Elijah Beach",
+	},
+	{
+		"list": "9",
+		"name": "Mercedes Humphrey",
+	},
+	{
+		"list": "5",
+		"name": "Dominic Koch",
+	},
+	{
+		"list": "1",
+		"name": "Baxter Hicks",
+	},
+	{
+		"list": "19",
+		"name": "Kellie Goodwin",
+	},
+	{
+		"list": "9",
+		"name": "Keiko Chandler",
+	},
+	{
+		"list": "5",
+		"name": "Sara Peck",
+	},
+	{
+		"list": "9",
+		"name": "Quin Carrillo",
+	},
 }
